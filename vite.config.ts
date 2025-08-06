c5fd05e6df4d8691ae88550fd9bde42b9119b7db
tomasdelvaso-crypto <tomasdelvaso@gmail.com>
2025-08-06 12:50:59 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  esbuild: {
+    // Ignorar TypeScript completamente
+    loader: 'jsx',
+    include: /\.[jt]sx?$/,
+    exclude: []
+  }
 })
