344e820f6209092a638f524ed1cd2a89247cceb5
tomasdelvaso-crypto <tomasdelvaso@gmail.com>
2025-08-06 15:20:06 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'bounce': 'bounce 1s infinite',
+        'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'spin': 'spin 1s linear infinite',
+      }
+    },
   },
   plugins: [],
 }
