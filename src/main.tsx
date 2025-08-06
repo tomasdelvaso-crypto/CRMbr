38405f97ed5f72e83410c09d609611c8e1c063da
tomasdelvaso-crypto <tomasdelvaso@gmail.com>
2025-08-06 06:26:35 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import CRMVentapel from './CRMVentapel'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CRMVentapel from './CRMVentapel';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <CRMVentapel />
   </React.StrictMode>,
-)
+);
