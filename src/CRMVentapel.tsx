38da8a9bd44f1c597422e80ed3154cc997dbcab9
tomasdelvaso-crypto <tomasdelvaso@gmail.com>
2026-03-19 09:46:34 -0300

    Remove redundant closing div tag
    Removed an unnecessary closing div tag.

--- src/CRMVentapel.tsx
+++ src/CRMVentapel.tsx
@@ -2191,8 +2191,6 @@ const CRMVentapel: React.FC = () => {
               </div>
             </div>
 
-       </div>
-
             {/* Activity Panel - solo al editar */}
             {opportunity && (
               <div className="mt-6">
